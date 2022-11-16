dc13766b37c686a17e71309bee7144963a582e47
JohanPOO <71298444+JohanPOO@users.noreply.github.com>
2022-11-16 15:56:23 -0500

    Comentarios en clase producto

--- src/main/java/com/example/entidades/Producto.java
+++ src/main/java/com/example/entidades/Producto.java
@@ -20,31 +20,38 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity(name = "ProductoEntity")
 @Table(name = "producto")
+//Clase Producto
 public class Producto implements Serializable{
 
     private static final long serialVersionUID = 1L;
     
+    //Variable Codigo
     @Id
     @Column(name = "codigo_producto")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long codigo;
     
+    //Variable Nombre
     @Column(name = "nombre_producto")
     private String nombre;
     
+    //Variable Precio
     @Column(name = "precio_producto")
     private double precio;    
     
     @Column(name = "stock_producto")
     private int stock;   
     
+    //Variable Stock
     @Column(name = "estado_producto")
     private boolean estado; 
     
+    //Foreign key proveedor
     @ManyToOne
     @JoinColumn(name = "codigo_proveedor", nullable = false)
     private Proveedor proveedor; 
     
+    //Foreign key categoria
     @ManyToOne
     @JoinColumn(name = "codigo_categoria", nullable = false)
     private Categoria categoria; 
